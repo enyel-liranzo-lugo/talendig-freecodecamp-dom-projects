888a6add4d5e1e2fee69478cdcab2500ab574fdc
Enyel Liranzo <xardimodschannel@gmail.com>
2024-02-26 17:08:15 -0400

    Adding button and styles

--- 01-hexadecimal-random-color/src/App.jsx
+++ 01-hexadecimal-random-color/src/App.jsx
@@ -1,6 +1,13 @@
 export default function App() {
 
   return (
-    <h1>Hello World!</h1>
+    <section className="flex flex-col text-center gap-y-5 font-semibold">
+      <h1 className="text-3xl">Random Hexadecimal Color Generator</h1>
+      <p className="text-xl">Current Color: <span className="">#ffffff</span></p>
+
+      <button className="bg-white w-fit mx-auto text-black p-2 rounded-lg border-slate-950 hover:bg-slate-700 hover:text-white transition-colors">
+        Change Color!
+      </button>
+    </section>
   )
 }
