8912459f379e451f0fb94f7108fe95177fbd6089
Enyel Liranzo <xardimodschannel@gmail.com>
2024-02-26 20:15:04 -0400

    Updating GneretateColorCard Logic and Design

--- 01-hexadecimal-random-color/src/components/GenerateColorCard.jsx
+++ 01-hexadecimal-random-color/src/components/GenerateColorCard.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prop-types
 export function GenerateColorCard({ currentColor, setFunction }) {
   return (
-    <section className={`lex flex-col text-center gap-y-5 font-semibold bg-white text-slate-800 p-5 rounded-lg w-[350px] md:w-auto`}>
+    <section className={`flex flex-col text-center gap-y-5 font-semibold bg-white text-slate-800 p-5 border-2 border-black rounded-lg w-[350px] md:w-auto`}>
       <h1 className="text-3xl font-bold">Random Hexadecimal Color Generator</h1>
       <p className="flex flex-col text-xl">Current Color: <span className="my-3 text-3xl">{currentColor}</span></p>
 
